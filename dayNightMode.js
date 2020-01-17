1d31826bfdd3f4e50265a1eaca7e497bb6c9e766
Jason Ko <jekb2019@gmail.com>
2020-01-17 17:49:31 +1300

    Refactoring: Used set colors

--- dayNightMode.js
+++ dayNightMode.js
@@ -1,27 +1,38 @@
 var Body = {
-  dayNightMode:function(self){
-    var bodyTag = document.querySelector('body');
-    if(self.value === 'Night mode'){
-      bodyTag.style.backgroundColor = 'black';
-      bodyTag.style.color = 'white';
-      self.value = 'Day mode';
-      Link.setColor('powderblue');
-    }else{
-      bodyTag.style.backgroundColor = 'white';
-      bodyTag.style.color = 'black';
-      self.value = 'Night mode';
-      Link.setColor('blue');
-    }
+  setBackgroundColor:function(color){
+    $('body').css('backgroundColor', color);
+    // document.querySelector('body').style.backgroundColor = color;
+  }
+
+  setColor:function(color){
+    $('body').css('color', color);
+    // document.querySelector('body').style.color = color;
   }
 }
 
 var Link = {
   setColor:function(color){
-    var alist = document.querySelectorAll('a');
-    var i=0;
-    while(i < alist.length){
-      alist[i].style.color = color;
-      i++;
-    }
+
+    $('a').css('color',color);
+    // var alist = document.querySelectorAll('a');
+    // var i=0;
+    // while(i < alist.length){
+    //   alist[i].style.color = color;
+    //   i++;
+    // }
   }
+}
+
+function dayNightMode(self){
+  var bodyTag = document.querySelector('body');
+  if(self.value === 'Night mode'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    Link.setColor('powderblue');
+    self.value = 'Day mode';
+  }else{
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    Link.setColor('blue');
+    self.value = 'Night mode';
 }
