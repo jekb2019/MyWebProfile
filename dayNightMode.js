2d515d7e0d99f9c380fd9ff5995d2628afe2137c
Jason Ko <jekb2019@gmail.com>
2020-01-19 11:27:21 +1300

    Bug Fixed: day and night mode button is now working

--- dayNightMode.js
+++ dayNightMode.js
@@ -2,13 +2,12 @@ var Body = {
   setBackgroundColor:function(color){
     $('body').css('backgroundColor', color);
     // document.querySelector('body').style.backgroundColor = color;
-  }
-
+  },
   setColor:function(color){
     $('body').css('color', color);
+  }
     // document.querySelector('body').style.color = color;
   }
-}
 
 var Link = {
   setColor:function(color){
@@ -25,14 +24,15 @@ var Link = {
 
 function dayNightMode(self){
   var bodyTag = document.querySelector('body');
-  if(self.value === 'Night mode'){
-    Body.setBackgroundColor('black');
-    Body.setColor('white');
-    Link.setColor('powderblue');
-    self.value = 'Day mode';
-  }else{
-    Body.setBackgroundColor('white');
-    Body.setColor('black');
-    Link.setColor('blue');
-    self.value = 'Night mode';
+    if(self.value === 'Night mode'){
+      self.value = 'Day mode';
+      Body.setBackgroundColor('black');
+      Body.setColor('white');
+      Link.setColor('powderblue');
+    }else{
+      self.value = 'Night mode';
+      Body.setBackgroundColor('white');
+      Body.setColor('black');
+      Link.setColor('blue');
+    }
 }
